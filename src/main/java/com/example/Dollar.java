1117a9139fb2d2b55621f1f2d4ba3501cb0cd5d4
smpark <smpark@fimasia.co.kr>
2024-11-13 09:41:04 +0900

    Dollar Test 추가

---
+++ src/main/java/com/example/Dollar.java
@@ -0,0 +1,14 @@
+package com.example;
+
+public class Dollar {
+    int amount;
+
+    Dollar(int amount){
+
+    }
+
+    void times(int multiplier){
+
+    }
+
+}
---
+++ src/test/java/com/example/DollarTest.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DollarTest {
+    @Test
+    public void testMultiplication(){
+        Dollar five = new Dollar(5);
+        five.times(2);
+        assertEquals(10, five.amount);
+    }
+}
