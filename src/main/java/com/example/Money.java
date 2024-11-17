792a2882742295e42dd74bea21059fb24a7009e1
uniinu1 <qkrtnals3636@likelion.org>
2024-11-18 00:05:01 +0900

    TDD 8장 - 팩토리 메서드 도입

--- src/main/java/com/example/Money.java
+++ src/main/java/com/example/Money.java
@@ -1,11 +1,21 @@
 package com.example;
 
-public class Money {
+public abstract class Money {
     protected int amount;
+
+    abstract Money times(int multiplier);
 
     public boolean equals(Object object){
         Money money = (Money) object;
         // 자바 객체 용어보다는 재정 용어를 사용하고 싶지만 통화 개념 없음
         return amount == money.amount && getClass().equals(money.getClass());
+    }
+
+    static Money dollar(int amount){
+        return new Dollar(amount);
+    }
+
+    static Money franc(int amount){
+        return new Franc(amount);
     }
 }
